c6cc34b509697547d10b7132f64d872d40c1a8d0
emmanuel2-s <cosmasemmanuel40@gmail.com>
2023-11-26 21:11:15 +0100

    some change on the image folder

--- src/images.jsx
+++ src/images.jsx
@@ -24,7 +24,7 @@ import chick_3 from './imgs/chicken 3.jpg'
 import chick_4 from './imgs/chicken 4.jpg'
 
 //categories//
-import pizza_icon from './imgs/pizza icon.png'
+import pizza_icon from './imgs/pizza.png'
 import food_icon from './imgs/Mayor Fast.png'
 import chicken_bucket from './imgs/fried chicken.png'
 import badge from './imgs/badge_prize.png'
